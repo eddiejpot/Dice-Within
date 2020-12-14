0522d50d7a89962b0226878a6dcbe3e5e718f423
eddiejpot <jamesporteredward@gmail.com>
2020-12-14 08:39:55 +0800

    lucky 11 game

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// Global variables
+var gameCounter = 0;
+
+// Dice roll function
+var rollDice = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.floor(randomDecimal);
+  var diceNumber = randomInteger + 1;
+  return diceNumber;
+};
+
+// Main function
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // Roll dice one and two
+  diceRollOne = rollDice();
+  diceRollTwo = rollDice();
+  //
+  var myOutputValue = `Better luck next time! Dice one was ${diceRollOne}. Dice two was ${diceRollTwo}`;
+  // START GAME
+  if ((diceRollOne == 5) && (diceRollTwo == 6)) {
+    myOutputValue = `Lucky 11! Dice one was ${diceRollOne}. Dice two was ${diceRollTwo}`;
+  }
+  // Add to game counter
+  gameCounter += 1;
+  // Output
+  return myOutputValue
+  + '<br>' + 'Games Played:  ' + gameCounter;
 };
